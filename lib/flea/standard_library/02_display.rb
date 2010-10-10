593c239bfb3edb359211bcb76a7d363946b8c893
aarongough <aaron@aarongough.com>
2010-10-10 16:15:25 -0400

    Renamed print to display.

---
+++ lib/flea/standard_library/02_display.rb
@@ -0,0 +1,3 @@
+$standard_library.concat([
+  [:define, :display, [:__native_function, "print evaluate(list[0], env).inspect"]]
+])
---
+++ test/unit/standard_library/display_test.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_helper.rb'))
+
+class PrintTest < Test::Unit::TestCase
+
+  test "should print integer literal" do
+    old_stdout = $stdout
+    buffer = StringIO.new
+    $stdout = buffer
+    env = Flea.run([[:display, 1]])
+    assert_equal "1", buffer.string
+    $stdout = old_stdout
+  end
+  
+  test "should print list literal" do
+    old_stdout = $stdout
+    buffer = StringIO.new
+    $stdout = buffer
+    Flea.run([[:display, [:quote, 1, 2, 3]]])
+    assert_equal "[1, 2, 3]", buffer.string
+    $stdout = old_stdout
+  end
+  
+end
