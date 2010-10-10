b68f004083b2d3ba6120d7b9ac20a1206fb41f25
aarongough <aaron@aarongough.com>
2010-10-10 16:37:49 -0400

    Changed file structure to distinguish between core procedures and those in the standard library.

--- lib/flea.rb
+++ lib/flea.rb
@@ -1,7 +1,8 @@
+Dir[File.join(File.dirname(__FILE__), 'flea', 'outer_core', '*.rb')].each {|file| require File.expand_path(file) }
 Dir[File.join(File.dirname(__FILE__), 'flea', 'standard_library', '*.rb')].each {|file| require File.expand_path(file) }
 
 require "rubygems"
 require "sexpistol"
 
 require File.expand_path(File.join(File.dirname(__FILE__), "flea", "flea_environment.rb"))
-require File.expand_path(File.join(File.dirname(__FILE__), "flea", "flea.rb"))
+require File.expand_path(File.join(File.dirname(__FILE__), "flea", "flea_core.rb"))
