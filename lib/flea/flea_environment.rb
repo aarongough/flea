8255006dcea8c358d1af84ffbdbe82f38d5b064b
aarongough <aaron@aarongough.com>
2010-10-11 14:34:48 -0400

    Added built-in boolean data-types to global environment.

--- lib/flea/flea_environment.rb
+++ lib/flea/flea_environment.rb
@@ -1,10 +1,18 @@
 class FleaEnvironment < Hash
   def initialize( outer = nil )
     @outer = outer
+    add_globals if(outer.nil?)
   end
 
   def find(key)
     return self if(self.has_key?(key) || @outer.nil?)
     return @outer.find(key)
+  end
+  
+  def add_globals
+    self.merge!({
+      :"#t" => true,
+      :"#f" => false
+    })
   end
 end
