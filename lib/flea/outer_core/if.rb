3eaf3109bbfa108a748b18dca90f84b6222f75be
aarongough <aaron@aarongough.com>
2010-10-12 21:48:01 -0400

    Added tests and code for alternate clauses in if statements.

--- lib/flea/outer_core/if.rb
+++ lib/flea/outer_core/if.rb
@@ -1,3 +1,8 @@
 (define if (native_function "
-  evaluate(list[1], env) if(evaluate(list[0], env))
+  first_consequent = list[0]
+  if(evaluate(list[0], env))
+    evaluate(list[1], env)
+  else
+    evaluate(list[2], env) unless(list[2].nil?)
+  end
 "))
--- test/unit/outer_core/if_test.rb
+++ test/unit/outer_core/if_test.rb
@@ -2,12 +2,42 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_hel
 
 class IfTest < Test::Unit::TestCase
   
-  test "should define test to 1" do
+  test "should not execute consequent" do
     env = Flea.run('
+      (define test 0)
       (if false
-        (define test 1))
+        (set! test 1))
     ')
-    assert_equal(nil, env[:test])
+    assert_equal(0, env[:test])
+  end
+  
+  test "should execute consequent" do
+    env = Flea.run('
+      (define test 0)
+      (if true
+        (set! test 1))
+    ')
+    assert_equal(1, env[:test])
+  end
+  
+  test "should execute alternative" do
+    env = Flea.run('
+      (define test 0)
+      (if false
+        (set! test 1)
+        (set! test 2))
+    ')
+    assert_equal(2, env[:test])
+  end
+  
+  test "should not execute alternative" do
+    env = Flea.run('
+      (define test 0)
+      (if true
+        (set! test 1)
+        (set! test 2))
+    ')
+    assert_equal(1, env[:test])
   end
   
 end
