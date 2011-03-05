e43be19314aec2fe8e293b8c732b3fcbe0315d1d
aarongough <aaron@aarongough.com>
2011-03-05 18:16:58 -0500

    Updated implementation of (lambda) to allow definition of variadic functions.

--- lib/flea/standard_library/lambda.scm
+++ lib/flea/standard_library/lambda.scm
@@ -13,21 +13,40 @@
       end
     end
     
-    Proc.new() do |environment, arguments, interpreter|
-      sub_env = Flea::Environment.new(interpreter.current_environment)
-      if formals.is_a? Array
-        formals.each_index do |i|
-          sub_env.define(formals[i], arguments[i])
-        end
-      elsif formals.is_a? Symbol
-        sub_env.define(formals, arguments)
-      end
-      interpreter.current_environment = sub_env
+    sub_env = Flea::Environment.new(interpreter.current_environment)
+    
+    execute_body = Proc.new() do |body, environment, interpreter|
+      interpreter.current_environment = environment
       result = nil
       body.each do |expression|
         result = interpreter.evaluate(expression)
       end
-      interpreter.current_environment = sub_env.parent
+      interpreter.current_environment = environment.parent
       result
+    end    
+    
+    if formals.is_a?(Array) && formals.include?(:'.')
+      Proc.new() do |environment, arguments, interpreter|
+        args = arguments.dup
+        named_formals = formals.slice(0, formals.index(:'.'))
+        list_formal = formals[formals.index(:'.') + 1]
+        named_formals.each_index do |i|
+          sub_env.define(named_formals[i], args.shift)
+        end
+        sub_env.define(list_formal, args)
+        execute_body.call(body, sub_env, interpreter)
+      end
+    elsif formals.is_a? Array
+      Proc.new() do |environment, arguments, interpreter|
+        formals.each_index do |i|
+          sub_env.define(formals[i], arguments[i])
+        end
+        execute_body.call(body, sub_env, interpreter)
+      end
+    elsif formals.is_a? Symbol
+      Proc.new() do |environment, arguments, interpreter|
+        sub_env.define(formals, arguments)
+        execute_body.call(body, sub_env, interpreter)
+      end
     end
   "))
